a089c7900720b8ad10f94696caa2c7425c4aab88
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2024-10-14 19:42:15 +0600

    zz

--- Shopperoo-Backend/app.js
+++ Shopperoo-Backend/app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: 'https://frontend-test-hazel-rho.vercel.app/', // Replace with your frontend URL
   credentials: true, // Allow credentials (cookies, etc.)
 };
 
