328d3451045de8621d40033922e06fdce870ddcc
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2024-10-17 23:25:40 +0600

    local to vercel

--- Shopperoo-Frontend/vite.config.js
+++ Shopperoo-Frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
+// import { builderDevTools } from "@builder.io/dev-tools/vite";
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure this matches the directory specified in vercel.json
-  },
+  // plugins: [react(), eslint(), builderDevTools()],
+  plugins: [react(), eslint(), builderDevTools()],
 });
