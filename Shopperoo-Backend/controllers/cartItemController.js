7fd6bf59d8bbdac0c5a95761481d10d927dde2d7
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2024-10-22 03:21:23 +0600

    vercel

--- Shopperoo-Backend/controllers/cartItemController.js
+++ Shopperoo-Backend/controllers/cartItemController.js
@@ -18,7 +18,8 @@ exports.getMyCart = catchAsync(async (req, res, next) => {
   if (cartItems.length) {
     cartItems.map((cartItem) => {
       if (cartItem.image) {
-        cartItem.image = `${req.protocol}://${req.get('host')}/img/products/cover-image/${cartItem.image}`;
+        cartItem.image = `${req.protocol === 'https' ? 'https' : 'http'}://${req.get('host')}/img/products/cover-image/${cartItem.image}`;
+        // cartItem.image = `${req.protocol}://${req.get('host')}/img/products/cover-image/${cartItem.image}`;
       }
       totalPrice += cartItem.subtotal;
     });
