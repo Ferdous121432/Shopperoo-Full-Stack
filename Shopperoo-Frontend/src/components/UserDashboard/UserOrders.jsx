aed41ef84f5e05dc0372e8d77127151b5174aef4
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2024-10-28 04:57:12 +0600

    e4r

--- Shopperoo-Frontend/src/components/UserDashboard/UserOrders.jsx
+++ Shopperoo-Frontend/src/components/UserDashboard/UserOrders.jsx
@@ -27,50 +27,56 @@ export default function UserOrders() {
     fetchOrders();
   }, [state.token]);
 
-return (
+  return (
     <div className="flex min-w-[550px] flex-col gap-2 border-2 border-slate-400 px-5 py-10">
-        <div className="flex flex-row justify-between">
-            <h1 className="font-semibold text-slate-950">Order ID</h1>
-            <h1 className="font-semibold text-slate-950">Order Date</h1>
-            <h1 className="font-semibold text-slate-950">Amount</h1>
-            <h1 className="font-semibold text-slate-950">Status</h1>
-            <h1 className="font-semibold text-slate-950">Invoice</h1>
-        </div>
-        <div className="relative py-1">
-            <span className="absolute bottom-0 left-0 h-0.5 w-full scale-x-100 bg-yellow-primary"></span>
-        </div>
-        <div>
-            {orders.length === 0 ? (
-                <div>No orders</div>
-            ) : (
-                orders.map((order) => (
-                    {orders.map((order) => (
-                        <div key={order._id} className="flex flex-row justify-between py-2">
-                          <h1 className="font-semibold text-slate-950">
-                            {order._id.slice(-8)}
-                          </h1>
-                          <h1 className="font-semibold text-slate-950">
-                            {new Date(order.createdAt).toLocaleDateString("en-GB", {
-                              day: "2-digit",
-                              month: "short",
-                              year: "numeric",
-                            })}
-                          </h1>
-                          <h1 className="font-semibold text-slate-950">
-                            {order.total_price}
-                          </h1>
-                          <h1 className={order.status==="delivered"?"font-semibold text-emarald-primary":"font-semibold text-slate-950"}>{order.status}</h1>
-                          <Link
-                            to={order.invoice_pdf}
-                            className="font-semibold text-slate-950"
-                          >
-                            Invoice
-                          </Link>
-                        </div>
-                      ))}
-                ))
-            )}
-        </div>
+      <div className="flex flex-row justify-between">
+        <h1 className="font-semibold text-slate-950">Order ID</h1>
+        <h1 className="font-semibold text-slate-950">Order Date</h1>
+        <h1 className="font-semibold text-slate-950">Amount</h1>
+        <h1 className="font-semibold text-slate-950">Status</h1>
+        <h1 className="font-semibold text-slate-950">Invoice</h1>
+      </div>
+      <div className="relative py-1">
+        <span className="absolute bottom-0 left-0 h-0.5 w-full scale-x-100 bg-yellow-primary"></span>
+      </div>
+      <div>
+        {orders.length === 0 ? (
+          <div>No orders</div>
+        ) : (
+          orders.map((order) => (
+            <div key={order._id} className="flex flex-row justify-between py-2">
+              <h1 className="font-semibold text-slate-950">
+                {order._id.slice(-8)}
+              </h1>
+              <h1 className="font-semibold text-slate-950">
+                {new Date(order.createdAt).toLocaleDateString("en-GB", {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </h1>
+              <h1 className="font-semibold text-slate-950">
+                {order.total_price}
+              </h1>
+              <h1
+                className={
+                  order.status === "delivered"
+                    ? "font-semibold text-emarald-primary"
+                    : "font-semibold text-slate-950"
+                }
+              >
+                {order.status}
+              </h1>
+              <Link
+                to={order.invoice_pdf}
+                className="font-semibold text-slate-950"
+              >
+                Invoice
+              </Link>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-);
+  );
 }
